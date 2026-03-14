c2e389a15e15a6967dba37a6212c250ef98d6fd8
AlexSemak <semakaleksandr2014@gmail.com>
2026-03-14 15:27:37 +0200

    feat: add changes for intentional merge conflict 4
    Yup, there it is

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      yup
       <h1 className="from-main-branch">From main branch yup</h1>
-
       <h1 className="from-main-branch">From main branch 2 yup</h1>
+      <h3 className="h3">Ypd</h3>
     </div>
   );
 }
